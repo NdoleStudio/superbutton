63359e59f8f44e4870a448adacfa7b144ba837e2
Acho Arnold <arnold@ndolestudio.com>
2022-12-25 16:35:28 +0200

    Add clarity

--- web/static/superbutton.js
+++ web/static/superbutton.js
@@ -6,4 +6,15 @@ window.SB_PROJECT_ID = 'df2ab94d-4201-4478-95ca-24e9173380c0'
   s.src = 'https://cdn.superbutton.app/widget.js'
   s.async = true
   d.getElementsByTagName('head')[0].appendChild(s)
-})()
+})()(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      ;(c[a].q = c[a].q || []).push(arguments)
+    }
+  t = l.createElement(r)
+  t.async = 1
+  t.src = 'https://www.clarity.ms/tag/' + i
+  y = l.getElementsByTagName(r)[0]
+  y.parentNode.insertBefore(t, y)
+})(window, document, 'clarity', 'script', 'ev08rutb6d')
